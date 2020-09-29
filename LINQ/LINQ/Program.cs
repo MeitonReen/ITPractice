dfabe4e44ec20a569798e0bbf56897c740010bc6
MeitonReen <meitonreen@gmail.com>
2020-09-29 17:00:33 +0300

    LINQ_Task1

--- LINQ/LINQ/Program.cs
+++ LINQ/LINQ/Program.cs
@@ -6,6 +6,23 @@ namespace LINQ
 {
 	class Program
 	{
+		/*
+		 * Дана целочисленная последовательность, содержащая как положительные,
+		 * так и отрицательные числа. Вывести ее первый положительный элемент и
+		 * последний отрицательный элемент.
+		 */
+		struct TwoValue
+		{
+			public int? FirstPositive;
+			public int? LastNegative;
+		}
+		static TwoValue GetFirstPositiveAndLastNegativeElement(IEnumerable<int> enumerableInt)
+		{
+			TwoValue Res;
+			Res.FirstPositive = enumerableInt.FirstOrDefault(El => El > 0);
+			Res.LastNegative = enumerableInt.LastOrDefault(El => El < 0);
+			return Res;
+		}
 		
 		static void Main(string[] args)
 		{
